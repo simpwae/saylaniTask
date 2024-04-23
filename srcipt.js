8e0d122abff9a05ff7fb31232588c5d7dd271abe
simpwae <naqashamjad777@gmail.com>
2024-04-23 14:39:17 +0500

    ...

---
+++ srcipt.js
@@ -0,0 +1,9 @@
+let submit = document.querySelector("#Submit");
+submit.addEventListener("click", () => {
+  let roll = document.querySelector("#roll");
+  let name = document.querySelector("#name");
+  let batch = document.querySelector("#batch");
+  let section = document.querySelector("#section");
+
+  //   window.location.href = ;
+});
---
+++ srcipt3.js
@@ -0,0 +1,5 @@
+let ans1 = localStorage.getItem("marks");
+let h1 = document.querySelector(".h1");
+h1.innerText = "Your Score is::" + ans1;
+
+console.log(ans1);
