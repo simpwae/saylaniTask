417be9b96b0c0bf52e92bb00b207f54185dd41d4
simpwae <naqashamjad777@gmail.com>
2024-04-23 15:16:51 +0500

    '..'

--- srcipt.js
+++ srcipt.js
@@ -1,9 +1,13 @@
 let submit = document.querySelector("#Submit");
 submit.addEventListener("click", () => {
-  let roll = document.querySelector("#roll");
-  let name = document.querySelector("#name");
-  let batch = document.querySelector("#batch");
-  let section = document.querySelector("#section");
+  let roll = document.querySelector("#roll").value;
+  let name = document.querySelector("#name").value;
+  let batch = document.querySelector("#batch").value;
+  let section = document.querySelector("#section").value;
+  localStorage.setItem("roll", roll);
+  localStorage.setItem("name", name);
+  localStorage.setItem("batch", batch);
+  localStorage.setItem("section", section);
 
   //   window.location.href = ;
 });
